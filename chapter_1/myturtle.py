f4c8190d6b1834ed08d0df9e986f40854e3c5aee
saliveja <esman@riseup.net>
2022-05-12 19:10:05 +0200

    using turtle, right and forward

---
+++ chapter_1/myturtle.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+forward(100)
+# forward 100 pixels
+shape('turtle')
+# shape() allows us to change the shape of the arrow
+# this arrow has the shape of a tiny turtle
+right(45)
+# right 45 degrees
+forward(150)
+# forward 150 pixels
