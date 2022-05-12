7f3d8f1147fd0488785b626feb33273e67e3ef73
saliveja <esman@riseup.net>
2022-05-12 19:58:29 +0200

    drawing square with for loop

--- chapter_1/myturtle.py
+++ chapter_1/myturtle.py
@@ -1,11 +1,6 @@
 from turtle import *
 
-forward(100)
-# forward 100 pixels
 shape('turtle')
-# shape() allows us to change the shape of the arrow
-# this arrow has the shape of a tiny turtle
-right(45)
-# right 45 degrees
-forward(150)
-# forward 150 pixels
+for i in range(4):
+    forward(100)
+    right(90)
