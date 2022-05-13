4792573d46a5dfc5360b8a285bb1b890a438431e
saliveja <esman@riseup.net>
2022-05-13 13:35:03 +0200

    finding average in list

---
+++ chapter_2/exercise_2_2_finding_the_average.py
@@ -0,0 +1,5 @@
+numlist = [53, 28, 54, 84, 65, 60, 22, 93, 62, 27, 16, 25, 74,
+           42, 4, 42, 15, 96, 11, 70, 83, 97, 75]
+
+def findAverage(numlist):
+    return sum(numlist) / len(numlist)
