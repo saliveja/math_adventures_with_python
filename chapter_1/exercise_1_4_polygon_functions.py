bd713f69e7c6171cff438f800bf989fc23c93117
saliveja <esman@riseup.net>
2022-05-13 09:59:42 +0200

    making a polygon function

---
+++ chapter_1/exercise_1_4_polygon_functions.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+def polygon(sidelength=100):
+    for i in range(6):
+        right(60)
+        forward(200)
+
+
+for i in range(120):
+    speed(0)
+    right(3)
+    polygon()
