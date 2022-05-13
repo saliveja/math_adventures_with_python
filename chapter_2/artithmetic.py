76d7fed0bf321d36fc44bfcc0a4de25467c234a6
saliveja <esman@riseup.net>
2022-05-13 13:21:57 +0200

    function for addition

--- chapter_2/artithmetic.py
+++ chapter_2/artithmetic.py
@@ -12,8 +12,18 @@ def mySum(num):
     running_sum = 0
     # starting value for running_sum is 0
     for i in range(1, num + 1):
+        # +1 for it to add 100 numbers not 99
         running_sum += i
         # adding i to the running_sum every loop
     return running_sum
 
 mySum(100)
+
+def mySum2(num):
+    running_sum = 0
+    for i in range(num+1):
+        running_sum += i ** 2 +1
+
+    return running_sum
+
+
---
+++ chapter_2/exercise_2_1_finding_the_sum.py
@@ -0,0 +1,6 @@
+
+def findSum(num):
+    total_sum = 0
+    for i in range(1, num+1):
+        total_sum += i
+    return total_sum
