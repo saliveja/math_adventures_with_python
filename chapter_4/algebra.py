7c29571f83ff14178a96aed4c240f4b433d05903
saliveja <esman@riseup.net>
2022-05-15 11:46:41 +0200

    applying formula

---
+++ chapter_4/algebra.py
@@ -0,0 +1,5 @@
+def equation(a, b, c, d):
+    """solves equations of the form ax + b = cx + d"""
+    return (d -b) / (a - c)
+
+equation(2, 5, 0, 13)
---
+++ chapter_4/exercise_4_1_solving_more_equations_for_x.py
@@ -0,0 +1,6 @@
+def equation(a, b, c, d):
+    """solves equations of the form ax + b = cx + d"""
+    return (d -b) / (a - c)
+
+equation(12, 18, -34, 67)
+# 1.065217391304348
