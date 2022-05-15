a1ed377dc3c5475a3ba4f445424084b42844ae56
saliveja <esman@riseup.net>
2022-05-15 11:30:13 +0200

    finding x

---
+++ chapter_4/plug.py
@@ -0,0 +1,13 @@
+def plug():
+    x = -100
+    # x is -100
+    while x < 100:
+        # while x is smaller than 100
+        if 2 * x + 5 == 13:
+            # if 2 multiplied by x + 5 is 13
+            print(f"x = {x}")
+            # x = whatever number os correct, in this case 4
+        x += 1
+        # this tests every number until finding the correct one
+
+plug()
